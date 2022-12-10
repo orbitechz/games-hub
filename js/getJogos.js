c1707e181fa0d695a7669c85e251195f17f5dcf5
Jean <jeanfelipe0610@gmail.com>
2022-12-10 12:36:45 -0300

    primeira implementacao ok

---
+++ js/getJogos.js
@@ -0,0 +1,16 @@
+
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6491e5bb7fmshf3fa2279323d6a7p1674e8jsndbbb08cdac8c',
+		'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+	}
+};
+
+const res = await fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?category=shooter', options).then(response => response.json());
+
+export function jogos() {
+	return res;
+}
+
